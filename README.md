4e6a57e9aa3eb6a7464463d1351fa2f7418561fd
Roman Zhigalov <larkie.wistful+github@gmail.com>
2020-04-22 12:52:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+sfdx-react-plugin
+======================
+
+SFDX plugin for React apps deployment on Salesforce
+
+[![Version](https://img.shields.io/npm/v/sfdx-react-plugin.svg)](https://npmjs.org/package/sfdx-react-plugin)
+[![Downloads/week](https://img.shields.io/npm/dw/sfdx-react-plugin.svg)](https://npmjs.org/package/sfdx-react-plugin)
+[![License](https://img.shields.io/npm/l/sfdx-react-plugin.svg)](https://github.com/rzhigalov/sfdx-react-plugin/blob/master/package.json)
+
+
+<div style="padding: 10px;background: rgba(227, 98, 9, 0.5);">
+  <div>⚠️ This package is yet in <b>development</b>.</div>
+</div>
+<!-- install -->
---
+++ src/commands/ngx/init.ts
@@ -0,0 +1,30 @@
+import { SfdxCommand } from '@salesforce/command';
+import { Messages, SfdxError } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages('sfdx-react-plugin', 'react');
+
+export default class ReactInit extends SfdxCommand {
+  public static description = messages.getMessage('commandDescription');
+
+  public static examples = [
+    `$ sfdx react:init
+
+    NOT IMPLEMENTED - WIP
+
+  `
+  ];
+
+  protected static flagsConfig = {};
+
+  protected static requiresProject = true;
+
+  public async run(): Promise<AnyJson> {
+    throw new SfdxError('NOT IMPLEMENTED - WIP');
+  }
+}
